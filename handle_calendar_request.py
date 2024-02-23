eeacaf363048d4da799a1ff1f3480c3eedf8e57b
yuens <yuens.me@gmail.com>
2024-02-23 12:20:12 -0500

    add comments for the code

--- handle_calendar_request.py
+++ handle_calendar_request.py
@@ -9,9 +9,12 @@ def handle_calendar_request(user_input, event_desc):
     # Step 2: Extract Event Details
     event_details = nlp_extract_details(user_input, event_desc=event_desc)
     print("from handle_calendar_request/event_details: ", event_details)
+
+    # TODO: loop thru all dates and times instead of hard coding
     start = f"{event_details['dates'][0]} {event_details['times'][0]}"
     end = f"{event_details['dates'][0]} {event_details['times'][0]}"
 
+    # TODO: add other basic calendar formats, like edit, list, etc
     if intent == "create_event":
         # Step 3: Use Google Calendar API to create the event
         create_event(
@@ -22,9 +25,6 @@ def handle_calendar_request(user_input, event_desc):
             location=event_details["locations"],
         )
         response = "Your event has been scheduled."
-    # elif intent == "query_events":
-    #     events = query_google_calendar_events(event_details)
-    #     response = format_events_for_display(events)
     else:
         response = "I'm not sure how to help with that."
 
